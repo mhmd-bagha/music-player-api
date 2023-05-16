fea2fc047adfed0125cc06ab4e162118ada0702d
mhmddev <mabrahimibagha@gmail.com>
2023-05-16 18:32:52 +0330

    feat: checkExistUser method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    //
+    public function login(Request $request, User $model)
+    {
+        $datavalidated = Validator::make($request->post(), [
+            'email' => 'required',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+    }
+
+    private function checkExistUser($email): bool
+    {
+        return User::where('email', $email)->first()->exists();
+    }
 }
