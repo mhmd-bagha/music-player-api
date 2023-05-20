5fb521ce1b2685a2028cfe444fa0f907e22c4c7c
mhmddev <mabrahimibagha@gmail.com>
2023-05-20 22:07:30 +0330

    feat: get user from redis

--- config/app.php
+++ config/app.php
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -183,6 +183,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Redis' => \Illuminate\Support\Facades\Redis::class
     ])->toArray(),
 
 ];
