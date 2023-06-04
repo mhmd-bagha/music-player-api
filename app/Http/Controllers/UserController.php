02023c26e974979a49a16ecf6b9c7e37ac517073
mhmddev <mabrahimibagha@gmail.com>
2023-06-04 22:12:43 +0330

    refactor: add json method to response

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
@@ -12,24 +13,24 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserController extends Controller
 {
-    public function login(Request $request, User $model): Response
+    public function login(Request $request, User $model): JsonResponse
     {
         $dataValidated = Validator::make($request->post(), [
             'email' => 'required',
             'password' => 'required|min:6'
         ]);
         if ($dataValidated->fails())
-            return response(['message' => $dataValidated->errors(), 'status' => 417], 417);
+            return response()->json(['message' => $dataValidated->errors(), 'status' => 417], 417);
 
         $dataValidated = $dataValidated->validated();
 
         if ($token = $this->checkExistUser($dataValidated))
-            return response(['message' => 'successfully login', 'token' => $this->createToken($token), 'status' => 200], 200);
+            return response()->json(['message' => 'successfully login', 'token' => $this->createToken($token), 'status' => 200]);
         else
             return $this->signup($dataValidated);
     }
 
-    public function signup($data): Response
+    public function signup($data): JsonResponse
     {
         // hashing password
         $data = array_merge($data, ['password' => Hash::make($data['password'])]);
@@ -37,12 +38,12 @@ class UserController extends Controller
         $jwtToken = JWTAuth::fromUser($createUser);
 
         if ($createUser)
-            return response(['message' => 'user created successfully', 'token' => $this->createToken($jwtToken, $createUser), 'status' => 201], 201);
+            return response()->json(['message' => 'user created successfully', 'token' => $this->createToken($jwtToken, $createUser), 'status' => 201], 201);
         else
-            return response(['message' => 'an error has occurred', 'status' => 422], 422);
+            return response()->json(['message' => 'an error has occurred', 'status' => 422], 422);
     }
 
-    public function getUser(): Response
+    public function getUser(): JsonResponse
     {
         if (!$this->getUserRedis()) {
 
@@ -54,7 +55,7 @@ class UserController extends Controller
             $user = $this->getUserRedis();
 
         }
-        return response(['user' => $user, 'status' => 200], 200);
+        return response()->json(['user' => $user, 'status' => 200]);
     }
 
     public function getUserRedis()
@@ -73,30 +74,30 @@ class UserController extends Controller
         return JWTAuth::attempt($data);
     }
 
-    public function refreshToken(): Response
+    public function refreshToken(): JsonResponse
     {
         // check don't exist response, authentication user with jwt
         $user = ($this->handleToken()) ? JWTAuth::authenticate() : [];
 
         if ($user) {
             $createToken = JWTAuth::fromUser($user);
-            return response(['token' => $this->createToken($createToken, $user), 'status' => 200], 200);
+            return response()->json(['token' => $this->createToken($createToken, $user), 'status' => 200]);
         } else {
-            return response(['message' => 'User not found', 'status' => 401], 401);
+            return response()->json(['message' => 'User not found', 'status' => 401], 401);
         }
     }
 
-    public function handleToken(): Response
+    public function handleToken(): JsonResponse
     {
         if (!request()->header('Authorization'))
-            return response(['message' => 'bearer token is required', 'status' => 500], 500);
+            return response()->json(['message' => 'bearer token is required', 'status' => 500], 500);
 
         // regenerate a new token
         try {
             JWTAuth::setToken(JWTAuth::refresh());
-            return response(['message' => 'Token refreshed successfully', 'status' => 200], 200);
+            return response()->json(['message' => 'Token refreshed successfully', 'status' => 200]);
         } catch (\Exception $e) {
-            return response(['message' => 'Failed to refresh token', 'status' => 401], 401);
+            return response()->json(['message' => 'Failed to refresh token', 'status' => 401], 401);
         }
     }
 
