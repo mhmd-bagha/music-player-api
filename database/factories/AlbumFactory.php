cb034b7285a47f098614d767e7c54e3ad754af14
mhmddev <mabrahimibagha@gmail.com>
2023-06-06 10:24:24 +0330

    feat: add album factory

---
+++ database/factories/AlbumFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Album;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Album>
+ */
+class AlbumFactory extends Factory
+{
+
+    protected $model = Album::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'singer_name' => fake()->name(),
+            'singer_image' => fake()->imageUrl(),
+            'singer_type' => 'playlist',
+        ];
+    }
+}
