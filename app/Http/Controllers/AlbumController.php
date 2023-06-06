5931bb54667c8926ebb2b256ca0ef711ad6ed06c
mhmddev <mabrahimibagha@gmail.com>
2023-06-06 11:09:38 +0330

    feat: add get albums

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Album;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AlbumController extends Controller
 {
-    //
+    public function albums(): JsonResponse
+    {
+        $albums = Album::all();
+        return response()->json(['data' => $albums, 'status' => 200]);
+    }
 }
