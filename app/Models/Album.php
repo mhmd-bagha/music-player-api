6e0142cf9cfd68f04fad718ff66cf4581f5aa919
mhmddev <mabrahimibagha@gmail.com>
2023-06-10 16:37:56 +0330

    feat: add relationship songs

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Album extends Model
 {
     use HasFactory;
+
     protected $fillable = ['singer_name', 'singer_image', 'singer_type'];
     protected $table = 'albums';
+
+    public function songs(): HasMany
+    {
+        return $this->hasMany(Song::class, 'album_id');
+    }
 }
