c8b0088e7b6a1b8c5402bc2cbad94484da5aae61
mhmddev <mabrahimibagha@gmail.com>
2023-06-18 12:45:47 +0330

    feat: add song liked

--- app/Http/Controllers/SongPopularController.php
+++ app/Http/Controllers/SongPopularController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\SongPopular;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SongPopularController extends Controller
 {
@@ -13,5 +14,21 @@ class SongPopularController extends Controller
         $songsLiked = SongPopular::where('user_id', $request->user_id)->get();
 
         return response()->json(['data' => $songsLiked, 'status' => 200]);
+    }
+
+    public function addSongLike(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->post(), [
+            'song_id' => 'required'
+        ]);
+        if ($validator->fails())
+            return response()->json(['message' => $validator->errors(), 'status' => 417], 417);
+
+        $songId = $validator->validated()['song_id'];
+        $data = [$songId, $request->user_id];
+
+        $addSongLike = SongPopular::create($data);
+
+        return ($addSongLike) ? response()->json(['message' => 'song added successfully', 'status' => 200]) : response()->json(['message' => 'an error has occurred', 'status' => 500]);
     }
 }
